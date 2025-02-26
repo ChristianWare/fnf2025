015a5b5916a4aaeafdcab2a57f5af5233bb9afff
Chris Ware <c.p.ware@hotmail.com>
2025-02-26 13:53:20 -0700

    Project section done

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -38,11 +38,25 @@ export default function Projects() {
           <span className={styles.includes}>Includes:</span>
           <ul className={styles.mapDataBox}>
             {data.map((x) => (
-              <li key={x.id} className={styles.card}>
-                {x.name}
+              <li key={x.id} className={styles.name}>
+                {x.name} <span className={styles.blackDot} />
               </li>
             ))}
           </ul>
+          <div className={styles.detailsBox}>
+            <div className={styles.detailsBoxLeft}>
+              <div className={styles.dblTop}>
+                <span className={styles.blackDot} /> COST
+              </div>
+              <h4 className={styles.detail}>15k</h4>
+            </div>
+            <div className={styles.detailsBoxRight}>
+              <div className={styles.dblTop}>
+                <span className={styles.blackDot} /> TIME
+              </div>
+              <h4 className={styles.detail}>30 days</h4>
+            </div>
+          </div>
         </div>
       </div>
     </section>
