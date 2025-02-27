21c38d590d99dd09c66fea1a30c391b1b30ba26a
Chris Ware <c.p.ware@hotmail.com>
2025-02-26 20:07:42 -0700

    ServicesSectionIntro

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -1,16 +1,23 @@
-import LayoutWrapper from '../LayoutWrapper'
-import styles from './Pricing.module.css'
+import LayoutWrapper from "../LayoutWrapper";
+import ServicesSectionIntro from "../ServicesSectionIntro/ServicesSectionIntro";
+import styles from "./Pricing.module.css";
+
+const cardsData = [
+  { id: 1, title: "5:" },
+  { id: 2, title: "Pricing" },
+];
 
 export default function Pricing() {
   return (
     <section className={styles.container}>
-        <LayoutWrapper>
-            <div className={styles.content}>
-                <h2 className={styles.heading}>
-                    Pricing
-                </h2>
-            </div>
-        </LayoutWrapper>
+      <ServicesSectionIntro data={cardsData} />
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <h3 className={styles.heading}>
+            Before you proceed, a few things you should know about us
+          </h3>
+        </div>
+      </LayoutWrapper>
     </section>
-  )
+  );
 }
