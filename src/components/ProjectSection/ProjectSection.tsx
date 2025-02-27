84e31eac063cba57ae4c8afeecb88dd29db9ed6e
Chris Ware <c.p.ware@hotmail.com>
2025-02-27 01:01:05 -0700

    ProjectSection done

--- src/components/ProjectSection/ProjectSection.tsx
+++ src/components/ProjectSection/ProjectSection.tsx
@@ -1,3 +1,4 @@
+import LayoutWrapper from "../LayoutWrapper";
 import ServicesSectionIntro from "../ServicesSectionIntro/ServicesSectionIntro";
 import styles from "./ProjectSection.module.css";
 
@@ -7,10 +8,34 @@ const cardsData = [
   { id: 3, title: "projects" },
 ];
 
+const projectData = [
+  { id: 1, title: "Project Name" },
+  { id: 2, title: "Project Name" },
+  { id: 3, title: "Project Name" },
+  { id: 3, title: "Project Name" },
+  { id: 3, title: "Project Name" },
+  { id: 3, title: "Project Name" },
+];
+
 export default function ProjectSection() {
   return (
     <section className={styles.container}>
       <ServicesSectionIntro data={cardsData} />
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.left}></div>
+          <div className={styles.right}>
+            <span className={styles.clients}>Clients (2013-2025)</span>
+            <div className={styles.projectDataContainer}>
+              {projectData.map((x) => (
+                <div key={x.id} className={styles.card}>
+                  <h3 className={styles.title}>{x.title}</h3>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </LayoutWrapper>
     </section>
   );
 }
