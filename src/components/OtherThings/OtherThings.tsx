1d449b67f706a04fe9de2fdfb044ebf1671ae64e
Chris Ware <c.p.ware@hotmail.com>
2025-02-26 19:50:49 -0700

    Otherthings updated

--- src/components/OtherThings/OtherThings.tsx
+++ src/components/OtherThings/OtherThings.tsx
@@ -2,7 +2,7 @@ import styles from "./OtherThings.module.css";
 import LayoutWrapper from "../LayoutWrapper";
 import SectionHeading from "../SectionHeading/SectionHeading";
 import Image from "next/image";
-import Img1 from "../../../public/images/glass2.png";
+import Img1 from "../../../public/images/glass.png";
 import Button from "../Button/Button";
 
 export default function OtherThings() {
@@ -15,6 +15,10 @@ export default function OtherThings() {
           </div>
           <div className={styles.bottom}>
             <div className={styles.left}>
+              <h3 className={styles.heading}>
+                In a world where decisions are made in a matter of seconds, you
+                only get one shot.
+              </h3>
               <div className={styles.imgContainer}>
                 <Image src={Img1} fill alt='' title='' className={styles.img} />
               </div>
@@ -22,6 +26,16 @@ export default function OtherThings() {
             <div className={styles.right}>
               <div className={styles.rightTop}>
                 <span className={styles.span}>Partners:</span>
+                <p className={styles.copy}>
+                  Whether you’re targeting investors, customers or users, you
+                  not only have to get to the point quickly, you have to give
+                  them a reason to believe you’re about to make their lives
+                  better, or at least make life itself better.
+                  <br />
+                  <br />
+                  Strong strategic core messaging is what lies behind that
+                  strong first impression.
+                </p>
               </div>
               <div className={styles.rightBottom}>
                 <h4 className={styles.info}>
