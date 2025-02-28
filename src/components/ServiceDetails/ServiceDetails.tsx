8ce2c30a72dd71938f5a54d68b29cf6e3fb759bc
Chris Ware <c.p.ware@hotmail.com>
2025-02-27 19:40:34 -0700

    ServiceDetails update

--- src/components/ServiceDetails/ServiceDetails.tsx
+++ src/components/ServiceDetails/ServiceDetails.tsx
@@ -36,19 +36,19 @@ const data = [
     title: "E-commerce Stores",
     company: "Self",
     description:
-      "Build modern and responsive websites using Next.js, and React. Proficient in designing and implementing custom solutions tailored to client needs, with a focus on delivering high-performance and SEO-friendly web experiences.",
+      "How do you give the world’s safest car seat a priceless personality?",
   },
   {
     title: "Business Websites",
     company: "KNI",
     description:
-      "Helped to build and maintain the following websites: https://aspiriant.com/, https://www.bolt.com/, https://www.rippling.com/",
+      "How do you give the world’s safest car seat a priceless personality?",
   },
   {
     title: "Booking Platforms",
     company: "Joyus App",
     description:
-      "2 month contract to help migrate the front end from Angular and Nest.js to Next.js. Worked with backend developers to integrate their api to the front end.",
+      "How do you give the world’s safest car seat a priceless personality?",
   },
   {
     icon: "",
@@ -59,13 +59,13 @@ const data = [
     title: "Landing Pages",
     company: "Mentorworks",
     description:
-      "Maintained current company income share agreement platform with the JavaScript, Node, React, and Python tech stack. Company went out of business.",
+      "How do you give the world’s safest car seat a priceless personality?",
   },
   {
     title: "Website Redesign",
     company: "Fandem",
     description:
-      "Helped to build the following NFT based websites: probablynothing.io, thebattlebunnies.io/, photosynthesis.io/",
+      "How do you give the world’s safest car seat a priceless personality?",
   },
   {
     title: "Resume",
@@ -84,7 +84,7 @@ export default function ServiceDetails() {
             <div className={styles.card} key={index}>
               {x.icon && <div className={styles.iconContainer}>{x.icon}</div>}
               <h3 className={styles.title}>{x.title}</h3>
-              <h4 className={styles.company}>{x.company}</h4>
+              {/* <h4 className={styles.company}>{x.company}</h4> */}
               <p className={styles.desc}>{x.description}</p>
               {x.btn && (
                 <div className={styles.btnContainer}>
