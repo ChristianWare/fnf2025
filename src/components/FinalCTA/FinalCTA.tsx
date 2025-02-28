1a20a1836c3cc81b84fa1abe5ac656d0408ad06b
Chris Ware <c.p.ware@hotmail.com>
2025-02-28 00:34:52 -0700

    final cta

--- src/components/FinalCTA/FinalCTA.tsx
+++ src/components/FinalCTA/FinalCTA.tsx
@@ -1,7 +1,22 @@
-import styles from './FinalCTA.module.css'
+import LayoutWrapper from "../LayoutWrapper";
+import styles from "./FinalCTA.module.css";
 
 export default function FinalCTA() {
   return (
-    <section className={styles.container}>FinalCTA</section>
-  )
+    <section className={styles.container}>
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <video preload='auto' autoPlay muted loop className={styles.video}>
+            <source src='./videos/glass.mp4' />
+          </video>
+          {/* <div className={styles.imgOverlay}></div> */}
+          <div className={styles.contentChildren}>
+            <h2 className={styles.heading}>
+              We Build <br /> Innovative <br /> Websites
+            </h2>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
 }
